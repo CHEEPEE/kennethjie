9ba2a26b25267ba6d67f43e777ad8b6c8bd531f4
jie <kennethjiepadasas@gmail.com>
2023-03-02 15:05:40 +0800

    AI page

---
+++ src/pages/ai.tsx
@@ -0,0 +1,9 @@
+import ChatBox from "~/components/ChatBox"
+
+const AiPage = () => {
+    return (<div className="flex relative items-center justify-center w-full h-[100vh]">
+        <ChatBox />
+    </div>)
+}
+
+export default AiPage
